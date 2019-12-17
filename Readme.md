2ed535ae3a601f166cbfa4ae93472d6f947c2e80
Mike H <matroxmike@icloud.com>
2019-12-17 21:23:17 +1100

    test and info in Readme

--- Readme.md
+++ Readme.md
@@ -2,4 +2,5 @@ example usage
 elixir  simple.exs   ; i.e. an exs file
 elixirc simple.ex    ; i.e. an ex  file
 iex                  ; To use the elixir shell use the iex command
+iex map1.exs
 
--- map1.exs
+++ map1.exs
@@ -2,9 +2,10 @@
 
 user = %{
   name: "John",
-  city: "Melbourne"
+  city: "Melbourne",
+  postcode: "3028"
 }
 
 IO.puts "Hello, " <> user.name
-
-
+IO.puts "Hello, " <> user.postcode
+#IO.puts "Hello, " <> user
